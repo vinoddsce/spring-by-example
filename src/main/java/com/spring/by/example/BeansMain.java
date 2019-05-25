3e9dbc7e79188b85ce9a4b88af1c959347553ea6
vinoddsce <vinod.dsce@gmail.com>
2019-05-26 04:13:31 +0530

    Adding Maven Dependencies - Basice

---
+++ src/main/java/com/spring/by/example/BeansMain.java
@@ -0,0 +1,18 @@
+package com.spring.by.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.by.example.beans.Student;
+
+
+public class BeansMain {
+	
+      public static void main(String[] args) {
+    	  
+             ApplicationContext appCon = new ClassPathXmlApplicationContext ("beans.xml");
+             Student factory = (Student)appCon.getBean("student");
+             factory.displayInfo();
+             
+      }
+}
--- src/main/java/com/spring/by/example/Test.java
+++
@@ -1,9 +0,0 @@
-package com.spring.by.example;
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
